ce7dd678a14fcdb412668fc32a2c4c53287bd149
fsantvoord <freek.van.santvoord@hva.nl>
2019-03-12 15:49:39 +0100

    Carousel werkt

--- opdracht2/v1/carousel.js
+++ opdracht2/v1/carousel.js
@@ -10,6 +10,9 @@ const dots = Array.from(dotsNav.children);
 const slideWidth = slides[0].getBoundingClientRect().width;
 
 // Arrange the slides next to on another
+//slides[0].style.left = slideWidth * 0 + 'px';
+//slides[1].style.left = slideWidth * 1 + 'px';
+//slides[2].style.left = slideWidth * 2 + 'px';
 
 const setSlidePosition = (slide, index) => {
         slide.style.left = slideWidth * index + 'px';
@@ -17,11 +20,27 @@ const setSlidePosition = (slide, index) => {
 
 slides.forEach(setSlidePosition);
 
-//slides[0].style.left = slideWidth * 0 + 'px';
-//slides[1].style.left = slideWidth * 1 + 'px';
-//slides[2].style.left = slideWidth * 2 + 'px';
-
+const moveToSlide = (track, currentSlide, targetSlide) => {
+    track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+    currentSlide.classList.remove('current-slide');
+    targetSlide.classList.add('current-slide');
+}
 
 // when I click left, move slides to the left
+prevButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const prevSlide = currentSlide.previousElementSibling;
+
+    moveToSlide(track, currentSlide, prevSlide);
+});
+
 // when I click right, move slides to the right
+nextButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const nextSlide = currentSlide.nextElementSibling;
+
+    moveToSlide(track, currentSlide, nextSlide);
+});
+
+
 // When I click the nav indicators, move to that slide
